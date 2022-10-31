9b88e5ffe9beb6012c3e6297fb819d696df7bf4a
ASiL010 <oguzasil86@gmail.com>
2022-11-01 01:19:27 +0300

    Şirket-Şahıs Şirketi Type XML format Added.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.CariKaydetBTN = new System.Windows.Forms.Button();
+            this.SirketSahıs = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(220, 94);
+            this.button1.Location = new System.Drawing.Point(42, 129);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(187, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Kaydetmeye Başlayın";
+            this.button1.Text = "Satış Siparişlerini Kaydet";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.KaydetBasla_Clk);
             // 
@@ -52,17 +54,40 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.KaydetmeYeri_Clk);
             // 
+            // CariKaydetBTN
+            // 
+            this.CariKaydetBTN.Location = new System.Drawing.Point(389, 129);
+            this.CariKaydetBTN.Name = "CariKaydetBTN";
+            this.CariKaydetBTN.Size = new System.Drawing.Size(207, 23);
+            this.CariKaydetBTN.TabIndex = 2;
+            this.CariKaydetBTN.Text = "Cari Hesapları Kaydet";
+            this.CariKaydetBTN.UseVisualStyleBackColor = true;
+            this.CariKaydetBTN.Click += new System.EventHandler(this.CariKaydetBTN_Clk);
+            // 
+            // SirketSahıs
+            // 
+            this.SirketSahıs.AutoSize = true;
+            this.SirketSahıs.Location = new System.Drawing.Point(389, 94);
+            this.SirketSahıs.Name = "SirketSahıs";
+            this.SirketSahıs.Size = new System.Drawing.Size(187, 20);
+            this.SirketSahıs.TabIndex = 3;
+            this.SirketSahıs.Text = "Şahıs Şirketleri için Seçiniz";
+            this.SirketSahıs.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SirketSahıs);
+            this.Controls.Add(this.CariKaydetBTN);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +95,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button CariKaydetBTN;
+        private System.Windows.Forms.CheckBox SirketSahıs;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace DatabaseToXml
         private DataSet ds;
         #endregion
         static SemaphoreSlim sem = new SemaphoreSlim(1, 1);
-        Thread t1;
+        Thread SatışSipariş,CariHesap;
         public Form1()
         {
             InitializeComponent();
@@ -28,19 +28,27 @@ namespace DatabaseToXml
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            t1 = new Thread(KaydetBaslaTread);
+            SatışSipariş = new Thread(SatıŞSiparisiKaydetBaslaTread);
+            CariHesap = new Thread(CariHesapKaydetBaslaTread);
         }
         string savingPath = Directory.GetCurrentDirectory();
         private void KaydetBasla_Clk(object sender, EventArgs e)
         {
 
-            if (t1.ThreadState == System.Threading.ThreadState.Unstarted)
-                t1.Start();
+            if (SatışSipariş.ThreadState == System.Threading.ThreadState.Unstarted)
+                SatışSipariş.Start();
             if (sem.CurrentCount == 0) sem.Release();
 
 
         }
 
+        private void CariKaydetBTN_Clk(object sender, EventArgs e)
+        {
+            if (CariHesap.ThreadState == System.Threading.ThreadState.Unstarted)
+                CariHesap.Start();
+            if (sem.CurrentCount == 0) sem.Release();
+
+        }
 
         private void KaydetmeYeri_Clk(object sender, EventArgs e)
         {
@@ -52,6 +60,136 @@ namespace DatabaseToXml
                 savingPath = Folder.SelectedPath;
             }
 
+        }
+        private void CariHesapKaydetBaslaTread()
+        {
+            while (true)
+            {
+                sem.Wait();
+                try
+                {
+                    if (SirketSahıs.Checked == true)
+                    {
+
+
+                        string sorgu = "SELECT  Distinct items_orderNumber  FROM [db_gulSistem].[dbo].[tbl_siparis]";
+                        #region AllNeededVaribles
+                        string[] CODE = stringArrayDoldur(sorgu);
+                        string[] TITLE = stringArrayDoldur(sorgu);
+                        string[] ADDRESS1 = stringArrayDoldur(sorgu);
+                        string[] TOWN = stringArrayDoldur(sorgu);
+                        string[] CITY = stringArrayDoldur(sorgu);
+                        string[] NAME = stringArrayDoldur(sorgu);
+                        string[] SURNAME = stringArrayDoldur(sorgu);
+                        string[] KVKK_BEGIN_DATE = stringArrayDoldur(sorgu);
+                        #endregion
+                        for (int i = 0; i < CODE.Length; i++)
+                        {
+
+
+
+                            XDocument doc = new XDocument(
+                           new XElement("AR_APS",
+                           new XElement("AR_AP",
+                                 new XElement("ACCOUNT_TYPE", 3),
+                                 new XElement("CODE", "AA990"+CODE[i]),
+                                 new XElement("TITLE", TITLE[i]),
+                                 new XElement("ADDRESS1", ADDRESS1[i]),
+                                 new XElement("TOWN_CODE", "TR"),
+                                 new XElement("TOWN", TOWN[i]),
+                                 new XElement("CITY_CODE", "TR"),
+                                 new XElement("CITY", CITY[i]),
+                                 new XElement("COUNTRY_CODE", "TR"),
+                                 new XElement("COUNTRY", "TÜRKİYE"),
+                                 new XElement("PERSCOMPANY", 1),
+                                 new XElement("TCKNO", 11111111111),
+                                 new XElement("EARCHIVE_SEND_MODE", 1),
+                                 new XElement("PROFILE_ID", 2),
+                                 new XElement("NAME", NAME[i]),
+                                 new XElement("SURNAME", SURNAME[i]),
+                                 new XElement("KVKK_BEGIN_DATE", KVKK_BEGIN_DATE[i])
+                           )//AR_AP end 
+                           )//AR_APSSSS end
+                           );//documant end
+
+
+                            doc.Declaration = new XDeclaration("1.0", "ISO-8859-9", "");
+
+                            doc.Save(savingPath + "\\" + CODE[i] + ".xml");
+                        }
+                    }
+                    else if(SirketSahıs.Checked==false)
+                    {
+                        //ŞİRKET HESAPLARI BURAYA GELECEK
+
+                        string sorgu = "SELECT  Distinct items_orderNumber  FROM [db_gulSistem].[dbo].[tbl_siparis]";
+
+                        #region AllNeededVaribles
+                        string[] CODE = stringArrayDoldur(sorgu);
+                        string[] TITLE = stringArrayDoldur(sorgu);
+                        string[] ADDRESS1 = stringArrayDoldur(sorgu);
+                        string[] DISTRICT = stringArrayDoldur(sorgu);
+                        string[] TOWN = stringArrayDoldur(sorgu);
+                        string[] CITY = stringArrayDoldur(sorgu);
+                        string[] TAX_ID = stringArrayDoldur(sorgu);
+                        string[] TAX_OFFICE = stringArrayDoldur(sorgu);
+                        string[] E_MAIL = stringArrayDoldur(sorgu);
+                        string[] CONTACT = stringArrayDoldur(sorgu);
+                        string[] CONTACT2 = stringArrayDoldur(sorgu);
+                        string[] NAME = stringArrayDoldur(sorgu);
+                        string[] SURNAME = stringArrayDoldur(sorgu);
+                        string[] POST_LABEL = stringArrayDoldur(sorgu);
+                        string[] SENDER_LABEL = stringArrayDoldur(sorgu);
+                        string[] EARC_EMAIL_ADDRESS1 = stringArrayDoldur(sorgu);
+                        #endregion
+                        for (int i = 0; i < CODE.Length; i++)
+                        {
+
+
+
+                            XDocument doc = new XDocument(
+                           new XElement("AR_APS",
+                           new XElement("AR_AP",
+                                 new XElement("ACCOUNT_TYPE", 3),
+                                 new XElement("CODE", "AA990" + CODE[i]),
+                                 new XElement("TITLE", TITLE[i]),
+                                 new XElement("ADDRESS1", ADDRESS1[i]),
+                                 new XElement("TOWN_CODE", "TR"),
+                                 new XElement("DISTRICT", DISTRICT[i]),
+                                 new XElement("TOWN", TOWN[i]),
+                                 new XElement("CITY_CODE", "TR"),
+                                 new XElement("CITY", CITY[i]),
+                                 new XElement("COUNTRY_CODE", "TR"),
+                                 new XElement("COUNTRY", "TÜRKİYE"),
+                                 new XElement("TAX_ID", TAX_ID[i]), 
+                                 new XElement("TAX_OFFICE", TAX_OFFICE[i]),
+                                 new XElement("E_MAIL", E_MAIL[i]),
+                                 new XElement("EARCHIVE_SEND_MODE", 1),
+                                 new XElement("PROFILE_ID", 2),
+                                 new XElement("CONTACT", CONTACT[i]),
+                                 new XElement("CONTACT2", CONTACT2[i]),
+                                 new XElement("NAME", NAME[i]),
+                                 new XElement("SURNAME", SURNAME[i]),
+                                 new XElement("POST_LABEL", POST_LABEL[i]),
+                                 new XElement("SENDER_LABEL", SENDER_LABEL[i]),
+                                 new XElement("EARC_EMAIL_ADDRESS1", EARC_EMAIL_ADDRESS1[i])
+                           )//AR_AP end 
+                           )//AR_APSSSS end
+                           );//documant end
+
+
+                            doc.Declaration = new XDeclaration("1.0", "ISO-8859-9", "");
+
+                            doc.Save(savingPath + "\\" + CODE[i] + ".xml");
+                        }
+                    }
+                }
+              
+                finally
+                {
+
+                }
+            }
         }
         public string[] stringArrayDoldur(string Sorgunuz)
         {
@@ -71,7 +209,7 @@ namespace DatabaseToXml
             return ReturnValue;
         }
 
-        private void KaydetBaslaTread()
+        private void SatıŞSiparisiKaydetBaslaTread()
         {
             while (true)
             {
@@ -80,7 +218,7 @@ namespace DatabaseToXml
                 {
                     string[] orderNumbers = stringArrayDoldur("SELECT Distinct items_orderNumber  FROM [db_gulSistem].[dbo].[tbl_siparis]");
                     for (int k = 0; k < orderNumbers.Length; k++)
-                    {//eşi olmayan veri sayısı
+                    {//eşi olmayan veri sayısı 56
                         string sorgu = "SELECT  Distinct items_orderNumber  FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k];
                        //her orderın içindeki veriler
                         string[] NUMBER = stringArrayDoldur(sorgu);
@@ -127,22 +265,27 @@ namespace DatabaseToXml
                             string[] DOC_TRACKING_NR = stringArrayDoldur(sorgu);
                             #endregion
 
-                            string[] amount = stringArrayDoldur("SELECT  items_totalPrice_amount FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k]);
+                            string[] Totalamount = stringArrayDoldur("SELECT  items_totalPrice_amount FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k]+ "order by items_orderNumber asc");
+                            string[] unitPrice = stringArrayDoldur("SELECT  items_unitPrice_amount FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k] + "order by items_orderNumber asc");
+                            string[] vatRate = stringArrayDoldur("SELECT  items_vatRate FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k] + "order by items_orderNumber asc");
+                            string[] quantity = stringArrayDoldur("SELECT  items_quantity FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k] + "order by items_orderNumber asc");
+                            string[] dueDate = stringArrayDoldur("SELECT  items_dueDate FROM [db_gulSistem].[dbo].[tbl_siparis] where items_orderNumber=" + orderNumbers[k] + "order by items_orderNumber asc");
+
                             //her orderın içindeki çoklu olabilen değer
-                            XElement[] a = new XElement[amount.Length];
+                            XElement[] a = new XElement[Totalamount.Length];
                             for (int j = 0; j < a.Length; j++)
                             {
                                 a[j] = new XElement("TRANSACTION");
-                                a[j].Add(new XElement("TYPE", "A"),
+                                a[j].Add(new XElement("TYPE", "0"),
                                      new XElement("MASTER_CODE", "A"),
-                                     new XElement("QUANTITY", "A"),
-                                     new XElement("PRICE", amount[j]),
-                                     new XElement("TOTAL", "A"),
-                                     new XElement("VAT_RATE", "A"),
+                                     new XElement("QUANTITY", quantity[j]),
+                                     new XElement("PRICE", unitPrice[j]),
+                                     new XElement("TOTAL", Totalamount[j]),
+                                     new XElement("VAT_RATE", vatRate[j]),
                                      new XElement("UNIT_CODE", "A"),
                                      new XElement("UNIT_CONV2", "A"),
                                      new XElement("ORDER_RESERVE", "A"),
-                                     new XElement("DUE_DATE", "A"),
+                                     new XElement("DUE_DATE", dueDate[j].Split(' ')[0]),
                                      new XElement("CURR_PRICE", "A"),
                                      new XElement("PC_PRICE", "A"),
                                      new XElement("RC_XRATE", "A"),
@@ -160,7 +303,7 @@ namespace DatabaseToXml
                            new XElement("ORDER_SLIP",
                                  new XElement("NUMBER", NUMBER[i]),
                                  new XElement("DOC_TRACK_NR", DOC_TRACK_NR[i]),
-                                 new XElement("DATE", DATE[i]),
+                                 new XElement("DATE", dueDate[i].Split(' ')[0]),
                                  new XElement("TIME", TIME[i]),
                                  new XElement("DOC_NUMBER", DOC_NUMBER[i]),
                                  new XElement("AUXIL_CODE", AUXIL_CODE[i]),
@@ -170,14 +313,8 @@ namespace DatabaseToXml
                                  new XElement("NOTES2", NOTES2[i]),
                                  new XElement("NOTES3", NOTES3[i]),
                                  new XElement("NOTES5", NOTES5[i]),
-                                 new XElement("ORDER_STATUS", ORDER_STATUS[i]),
-                                 new XElement("CREATED_BY", CREATED_BY[i]),
-                                 new XElement("DATE_CREATED", DATE_CREATED[i]),
-                                 new XElement("HOUR_CREATED", HOUR_CREATED[i]),
-                                 new XElement("MIN_CREATED", MIN_CREATED[i]),
-                                 new XElement("SEC_CREATED", SEC_CREATED[i]),
-                                 new XElement("MODIFIED_BY", MODIFIED_BY[i]),
-                                 new XElement("CURRSEL_TOTAL", CURRSEL_TOTAL[i]),
+                                 new XElement("ORDER_STATUS", "4"),
+                                 new XElement("CURRSEL_TOTAL", "2"),
                                 transactions
                                 ,//Transactions Altı
                                new XElement("CUST_ORD_NO", CUST_ORD_NO[i]),
@@ -201,6 +338,7 @@ namespace DatabaseToXml
                 }
             }
         }
+
     }
 
 
